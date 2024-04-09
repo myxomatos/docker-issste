00a959969eb3de324e658943cdc980802c88d72c
Aldo <myxomatos46@gmail.com>
2024-04-08 23:08:45 -0600

    Busqueda Directorio 2

---
+++ laravel-app/resources/views/admin/searchDirectorio.blade.php
@@ -0,0 +1,50 @@
+@extends('layout.home')
+
+@section('content')
+
+    <div uk-grid>
+        <div class="uk-visible@m uk-width-1-6@m">
+            @include('partials.sidebar')
+        </div>
+
+        <div class="uk-width-expand@m">
+            <div class="uk-text-center uk-margin-medium">
+                <a href="{{ route('directorio') }}">
+                    <button class="uk-margin-medium-left button_back" style="float: right;margin: 0px 40px 0px 0px">
+                        Volver
+                    </button>
+                </a>
+
+            </div>
+            <table class="uk-table uk-table-striped">
+                <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <!-- <th>Apellidos</th>
+                    <th>Genero</th>
+                    <th>Edad</th>
+                    <th>RFC</th>
+                    <th>Acciones</th> -->
+
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($usuario as $u)
+                    <tr>
+                        <td>
+                            {{ $u->name }}
+                        </td>
+                        
+                    </tr>
+                @endforeach
+                </tbody>
+
+            </table>
+        </div>
+    </div>
+
+
+
+@endsection
+
+
