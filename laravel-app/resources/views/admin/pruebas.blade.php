eb3580b671785d30829223e0a1d97c2599cbe668
Aldo <myxomatos46@gmail.com>
2024-06-30 19:23:34 -0600

    Se agrega la vista de pruevas

---
+++ laravel-app/resources/views/admin/pruebas.blade.php
@@ -0,0 +1,17 @@
+@extends('layout.home')
+
+@section('content')
+    <div uk-grid>
+        <div class="uk-visible@m uk-width-1-6@m">
+            @include('partials.sidebar')
+        </div>
+        <div class="uk-width-expand@m uk-margin-top uk-margin-right uk-margin-bottom">
+            <div class="uk-card uk-card-default uk-card-body card_counter uk-text-center">
+                <p class="color_7" style="font-size: 24px; margin: 0px;">{{ $incidencia_id }}</p>
+            </div>
+        </div>
+    </div>
+
+
+
+@endsection
